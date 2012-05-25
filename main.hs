49ea99ace483ff0cfd31277f9fff2d41b82e0bf3
Edgar Z. Alvarenga <edgar@ymonad.com>
2012-05-25 02:44:08 -0300

    Fixing bug in conway's vector creation. Writing the update world one step function

--- main.hs
+++ main.hs
@@ -1,5 +1,6 @@
 import qualified Data.Vector.Unboxed as V
 import qualified Data.Array.Unboxed as A
+import Control.Monad (forM_)
 
 type Binary = [Int]
 type Grid = A.UArray (Int,Int) Int
@@ -24,7 +25,9 @@ crule (s:ngb) | s == 1 = if cn == 2 || cn == 3 then 1 else 0
 -- The index represent a state S_ij(t - dt), N_ij(t - dt) in binary format, 
 -- the value at the index position is the S_ij(t) value 
 conway :: V.Vector Int 
-conway = V.fromList $ map (crule.binaryList) [0..512]
+conway = V.fromList $ map (crule.s_ngb) [0..512]
+    where s_ngb n = let bn = binaryList n in
+                    (replicate (9 - length bn) 0) ++ bn
 
 grid :: Int -> Grid
 grid n = A.array ((0,0), (n,n)) [((i,j), 0) | i <- [0..n], j <- [0..n]]
@@ -49,3 +52,14 @@ mkGlider g (x,y) = g A.// [((x-1, y-1), 0), ((x, y-1), 1), ((x+1, y-1), 0),
                          ((x-1, y), 0), ((x,y), 0), ((x+1, y), 1),
                          ((x-1, y+1), 1), ((x,y+1), 1), ((x+1, y+1), 1)]
 
+updateGrid :: Grid -> V.Vector Int -> Grid
+updateGrid g v = A.array bs [((i,j), up i j) | i <- [x0..x], j <- [y0..y]] 
+    where 
+        bs@((x0,y0), (x,y)) = A.bounds g
+        up i j = v V.! (b2i $ g A.! (i,j) : ngb g (i,j)) 
+
+printGrid :: Grid -> IO ()
+printGrid g = do
+    let bs@((x0,y0), (x,y)) = A.bounds g
+    forM_ [y0..y] $ \j -> do
+        print $ concatMap (\i -> show $ g A.! (i,j)) [x0..x]
